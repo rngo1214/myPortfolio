d83b993367764e6ccbe54b265d9cf05c786baf15
rngo1214 <rngo1214@gmail.com>
2018-10-17 01:31:56 -0700

    Added basic animation to menu button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 //Select DOM Items
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
